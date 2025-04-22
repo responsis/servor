595de1fb1fd0c823a76091b32cffe74bfd3323b1
Bertrand Caron <bertrand.caron@cascade.app>
2025-04-23 09:55:25 +1000

    Feat: Support providing header files using `--headers @/path/to/file` (similar to `curl`)

--- cli.js
+++ cli.js
@@ -72,6 +72,40 @@ const open =
     }
   }
 
+  const getHeaders = () => {
+    // See whether an argument was provided for the `--headers` flag
+    if (process.argv.indexOf('--headers') !== -1) {
+      const headersContent = process.argv[process.argv.indexOf('--headers') + 1];
+      if (headersContent.startsWith('@')) {
+        const headersFile = headersContent.slice(1);
+        // The header content is a file; try reading it (with the `@` sign removed)
+        try {
+          const fileContent = fs.readFileSync(headersFile);
+          // Try parsing the content as JSON
+          try {
+            return JSON.parse(fileContent);
+          } catch (error) {
+            console.error(error);
+            throw new Error(`Could not parse content of --headers file '${headersFile}' as JSON.`)
+          }
+        } catch (error) {
+          console.error(error)
+          throw new Error(`Could not read file '${headersFile}'`)
+        }
+      } else {
+        // The header content is assumed to be JSON, parse it.
+        try {
+          return JSON.parse(headersContent);
+        } catch (error) {
+          console.error(error);
+          throw new Error('Could not parse --headers content as JSON.')
+        }
+      }
+    } else {
+      return undefined
+    }
+  }
+
   // Parse arguments from the command line
 
   const { root, protocol, port, ips, url } = await servor({
@@ -81,7 +115,7 @@ const open =
     reload: !!~process.argv.indexOf('--reload'),
     module: !!~process.argv.indexOf('--module'),
     static: !!~process.argv.indexOf('--static'),
-    headers: process.argv.indexOf('--headers') !== -1 ? JSON.parse(process.argv[process.argv.indexOf('--headers') + 1]): undefined,
+    headers: getHeaders(),
     credentials,
   });
 
